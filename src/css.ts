6c59848a6f4880cf6cdac1495f1b726b4343a30b
Michael Meyer <9434150+mtmeyer@users.noreply.github.com>
2022-04-17 12:28:11 +1000

    refactor: updated css function & added release github action

--- src/css.ts
+++ src/css.ts
@@ -1,34 +1,34 @@
 import hash from "object-hash";
 import nested from "postcss-nested";
 import postcss from "postcss";
-import { setStyleSheet, rebuildCssString } from "./utils";
+import { setStyleSheet, rebuildCssString, removeEmptyLines } from "./utils";
 
 export const css = (strings: TemplateStringsArray, ...args: unknown[]) => {
-  const convertedString = strings.reduce(
-    (acc, string, index) =>
-      acc + string + (index < args.length ? args[index] : ""),
-    ""
-  );
+  // Convert strings array into string & inject args
+  let convertedString = "";
+  strings.forEach((item, index) => {
+    const arg = index < args.length ? args[index] : "";
+    convertedString = convertedString + item + arg;
+  });
 
   const parsedString = removeEmptyLines(convertedString);
 
+  // Create a class name using the hash of the css string
   let className = hash(parsedString);
   className = className.substring(0, 10);
   const css = `.${className} { ${parsedString} }`;
 
-  addToStyleSheet(css, className);
+  addToStyleSheet(css);
 
   return className;
 };
 
-const removeEmptyLines = (string: string) => {
-  return string.replace(/^\s*\n/gm, "");
-};
-
-const addToStyleSheet = (css: string, className: string) => {
+const addToStyleSheet = (css: string) => {
+  // Get style element
   const sheet = setStyleSheet();
   if (!sheet) throw new Error("Stylesheet not correctly mounted");
 
+  // Process nested css and add css to style element
   postcss([nested])
     .process(css, { from: undefined })
     .then((res) => {
